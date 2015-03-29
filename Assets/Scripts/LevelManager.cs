b45dd7bc7e06cf7e235c836b55a3516c393769d2
Erik Aulin <erik@aulin.co>
2015-03-29 18:33:58 +0200

    Assets, Fonts and Start Menu added

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("New Level load: "+ name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log("Quit requested");
+		Application.Quit();
+	}
+	
+	public void Higher(){
+		Debug.Log("Number is higher then: ");
+		Application.LoadLevel(name);
+	}
+	
+	public void Lower(){
+		Debug.Log("Number is lower then: ");
+		Application.LoadLevel(name);
+	}
+	
+	public void Equal(){
+		Debug.Log("You win!");
+		Application.LoadLevel(name);
+	}
+}
