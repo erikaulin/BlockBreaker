bc8de83c26e0d24135b05e9844a732d86af42230
Erik Aulin <erik@aulin.co>
2015-03-30 14:34:55 +0200

    Added Materials and fuctions to paddle and ball.

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LooseCollider : MonoBehaviour {
+public class LoseCollider : MonoBehaviour {
 
+	public LevelManager levelManager;
+	
 	void OnTriggerEnter2D (Collider2D trigger) {
 		print ("Trigger");
+		levelManager.LoadLevel("Win Screen");
+
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
