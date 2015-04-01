b18fcd4a6fa0ae34e4e48918ed86dbb76ebef230
Erik Aulin <erik@aulin.co>
2015-04-01 19:03:43 +0200

    Updated with new stuff

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 	
+	public AudioClip crack;
 	public Sprite[] hitSprites;
 	public static int breakableCount = 0;
 
@@ -28,10 +29,10 @@ public class Brick : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D (Collision2D col) {
+		AudioSource.PlayClipAtPoint (crack, transform.position);
 		if (isBreakable) {
 			HandleHits();
 		}
-
 	}
 
 	void HandleHits () {
