8072be036c8cb050ea62c4eee7d2e2ec9792bc45
Erik Aulin <erik@aulin.co>
2015-04-02 11:48:40 +0200

    Changed paddle to float value size

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Paddle : MonoBehaviour {
 
 	public bool autoPlay = false;
+	public float minX, maxX;
+	// public int maxX = 1.18f 14.8f 
 
 	private Ball ball;
 
@@ -23,7 +25,7 @@ public class Paddle : MonoBehaviour {
 	void AutoPlay() {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		Vector3 ballPos = ball.transform.position;
-		paddlePos.x = Mathf.Clamp(ballPos.x, 0.5f, 15.5f);
+		paddlePos.x = Mathf.Clamp(ballPos.x, minX, maxX);
 		this.transform.position = paddlePos;
 	}
 
@@ -31,7 +33,7 @@ public class Paddle : MonoBehaviour {
 	void MoveWithMouse () {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
 		this.transform.position = paddlePos;
 	}
 }
